d48b6ccaec544ccbd2e75499e02f6ea40a56f775
Jared Ricioli <jared.ricioli@live.com>
2024-05-27 05:56:44 -0300

    alterações na Home, construção de rotas e começo dos trabalhos na tela de perfil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SearchComponent } from "./search/search.component";
+import { SearchComponent } from "./components/search/search.component";
+import { PerfilComponent } from "./pages/perfil/perfil.component";
+
+
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, SearchComponent]
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, SearchComponent, PerfilComponent]
 })
 export class AppComponent {
   title = 'search-d_evs';
