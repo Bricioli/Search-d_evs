02c89b4da0633f1703d1680c4cc54786b79cf32c
Jared Ricioli <jared.ricioli@live.com>
2024-05-27 03:22:57 -0300

    feita a logica para buscar os valores da api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule} from '@angular/forms';
 import { SearchComponent } from "./search/search.component";
 
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, MatSlideToggleModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule, SearchComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, SearchComponent]
 })
 export class AppComponent {
   title = 'search-d_evs';
-  value = 'Clear me';
+
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -5,15 +5,44 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [MatSlideToggleModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule],
+  imports: [MatSlideToggleModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatIconModule, HttpClientModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
 
+
+export class SearchComponent {
+  public perfil: String | undefined;
+  public resultPerfil: any;
+  public resultPerfilRepos: any;
+  private readonly url : string = "https://api.github.com/users/"
+
+
+
+  constructor(private httpClient: HttpClient){}
+
+
+  search() {
+    console.log(this.perfil)  // valor inserido no input
+    
+    this.resultSearch()
+  }
+
+  resultSearch() {
+    this.httpClient.get(`${this.url}${this.perfil}`).subscribe(res => {
+      this.resultPerfil = res;
+      console.log(this.resultPerfil)
+    });
+    this.httpClient.get(`${this.url}${this.perfil}/repos`).subscribe(res => {
+      this.resultPerfil = res;
+      console.log(this.resultPerfil)
+    });
+  }
 }
